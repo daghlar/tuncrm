82f1267c8735c4a017ad93401475079dafb63441
TunCRM Developer <omarchy@tuncrm.com>
2025-09-07 17:36:04 +0300

    Simplify theme toggle to pure CSS approach
    - Removed all JavaScript dependencies
    - Theme toggle now uses pure Blazor state management
    - Added CSS class-based theming
    - Should be much more reliable

--- obj/Debug/net9.0/TunCRM.AssemblyInfo.cs
+++ obj/Debug/net9.0/TunCRM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TunCRM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5021657fda3f21741353e49328ecff7ff6c1b546")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c8b959211ceffb0b1921b7b48c0f0a23f421b40")]
 [assembly: System.Reflection.AssemblyProductAttribute("TunCRM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TunCRM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
